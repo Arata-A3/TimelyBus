77551a0fd1e722b7022e860efcf13f59f9e35e40
Arata-A3 <c612121014@gmail.com>
2026-04-13 00:13:37 +0900

    Delete sw.js

--- sw.js
+++
@@ -1,8 +0,0 @@
-self.addEventListener('install', (e) => {
-    self.skipWaiting();
-});
-
-self.addEventListener('fetch', (e) => {
-    // 最小限のパススルー（PWAインストール要件を満たすため）
-    e.respondWith(fetch(e.request));
-});
